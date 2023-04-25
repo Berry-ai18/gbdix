93fbf6df8ea3fdc5557dc951c3d12d196261efa6
Hung Do Quoc <hung.do.quoc@student.tuke.sk>
2023-04-25 13:54:06 +0200

    mobile version

--- js/general.js
+++ js/general.js
@@ -1,11 +1,14 @@
 const menu_btn = document.querySelector('.menu-btn');
+const menu_span = document.querySelectorAll('.menu > span');
+const menu = document.querySelector('.menu');
 const nav = document.querySelector('.navigation');
 const links = document.querySelectorAll('.navigation ul li');
-const menu_span = document.querySelectorAll('.menu > span');
+
 
 menu_btn.addEventListener('click', () => {
-    menu_btn.classList.toggle('open');
     nav.classList.toggle('open');
+    menu_btn.classList.toggle('open');
+    menu.classList.toggle('open');
     menu_span.forEach( (e) => {
         e.classList.toggle('open');
     })
